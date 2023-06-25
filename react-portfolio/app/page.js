9489de74576e96d1d679ca3a4ec9a8c6f13dcd29
erickadikah <erickadikah2030@gmail.com>
2023-06-25 13:33:34 +0300

    portfolio updates

--- react-portfolio/app/page.js
+++ react-portfolio/app/page.js
@@ -1,9 +1,13 @@
-"use client"
-import Head from 'next/head';
-import { BsFillMoonStarsFill } from 'react-icons/bs';
-import { AiFillTwitterCircle, AiFillGithub, AiFillLinkedin,} from 'react-icons/ai';
-import { FaEnvelope } from 'react-icons/fa'
-import Image from 'next/image';
+"use client";
+import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { FaEnvelope } from "react-icons/fa";
+import Image from "next/image";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
@@ -14,8 +18,9 @@ import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
 import deved from "../public/deved.png";
-import web7 from "../public/web7.png";
-import { useState } from 'react';
+import web10 from "../public/web10.png";
+import web8 from "../public/web8.png";
+import { useState } from "react";
 import { useTheme } from "next-themes";
 
 export default function Home() {
@@ -51,7 +56,7 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-            Erick Adikah   
+              Erick Adikah
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Back-End Developer and Graphics designer.
@@ -61,30 +66,35 @@ export default function Home() {
               needs. Join me down below and let&apos;s get cracking!
             </p>
             <div className="text-3xl flex justify-center gap-5 py-3 text-gray-600 dark:text-gray-400">
-            <a href="https://github.com/ErickAdikah ">
-            <AiFillGithub />
-            </a>
+              <a href="https://github.com/ErickAdikah ">
+                <AiFillGithub />
+              </a>
               <a href="https://twitter.com/https:/erick_adikah">
-            <AiFillTwitterCircle />
-            </a>
+                <AiFillTwitterCircle />
+              </a>
               <a href="mailto:erickadikah2030@gmail.com">
-            <FaEnvelope />
-            </a>
-            <a href="https://www.linkedin.com/in/erick-adikah-0b0b3a1b0/">
-              <AiFillLinkedin />
+                <FaEnvelope />
+              </a>
+              <a href="https://www.linkedin.com/in/erick-adikah-0b0b3a1b0/">
+                <AiFillLinkedin />
               </a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-40 h-40 relative overflow-hidden mt-20 md:h-60 md:w-60">
-            <Image alt="profile" src={deved} layout="fill" objectFit="cover"/>
-          </div>
+              <Image
+                alt="profile"
+                src={deved}
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
           </div>
         </section>
         <section>
-          <div className='mx-auto'>
+          <div className="mx-auto">
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance graphics designer and
-              developer. I&apos;ve done some graphics works for Companies
+              Since the beginning of my journey as a freelance graphics designer
+              and developer. I&apos;ve done some graphics works for Companies
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talanted people to create digital products
@@ -97,9 +107,15 @@ export default function Home() {
           </div>
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image alt="design" src={design} width={100} height={100} className='mx-auto'/>
+              <Image
+                alt="design"
+                src={design}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs  For Graphics Needs
+                Beautiful Designs For Graphics Needs
               </h3>
               <p className="py-2">
                 Creating elegant designs suited for your needs following core
@@ -112,12 +128,19 @@ export default function Home() {
               <p className="text-gray-800 py-1">Indesign</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image alt="code" src={code} width={100} height={100} className='mx-auto'/>
+              <Image
+                alt="code"
+                src={code}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
                 Code your dream project
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let&rsquo;s make it a reality.
+                Do you have an idea for your next great website? Let&rsquo;s
+                make it a reality.
               </p>
               <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
               <p className="text-gray-800 py-1">ReactJs</p>
@@ -127,7 +150,13 @@ export default function Home() {
               <p className="text-gray-800 py-1">NextJs</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image alt="consulting" src={consulting} width={100} height={100} className='mx-auto'/>
+              <Image
+                alt="consulting"
+                src={consulting}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
               <p className="py-2">
                 Are you interested in feedback for your current project? I can
@@ -145,16 +174,16 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a Freelancer Graphics designer and
-              developer, I&rsquo;ve done remote work for
+              Since the beginning of my journey as a Freelancer Graphics
+              designer and developer, I&rsquo;ve done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">startups </span>
               and collaborated with talented people to create digital products
               for both business and consumer use.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand Graphics design,
-              programming, and consulting.
+              I offer from a wide range of services, including brand Graphics
+              design, programming, and consulting.
             </p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
@@ -219,37 +248,97 @@ export default function Home() {
               />
             </div>
             <a href="https://art-gala2b2cbb.netlify.app/">
-            <div className="basis-1/2 flex-2">
-              <Image
-                className="w-full h-full object-contain justify-between"
-                width={"80%"}
-                height={"80%"}
-                layout="responsive"
-                src={web7}
-                alt="project7"
-              />
+              <div className="basis-1/2 flex-2">
+                <Image
+                  className="w-full h-full object-contain justify-between"
+                  width={"80%"}
+                  height={"80%"}
+                  layout="responsive"
+                  src={web10}
+                  alt="project7"
+                />
               </div>
             </a>
-            <div className='w-full x1:w-1/2 fle flex-col gap-6 
-          items-end text-right x1:-m-1-16 z-10'>
-          <p className='font-titleFont text-textGreen text-sm tracking-wide dark:text-white'
-          >Featured Project</p>
-          <h3 className='text-2xl font-bold dark:text-white'>Art Galary</h3>
-          <p className='bg-[#112240] text-sm md:text-base p-2 md:p-6 rounded-md'>
-          Art Galary is a website where you can find all the art work of your favorite artist.
-          and you can also buy the art work from the website.
-          you can also find the art work of your <span className='text-white'>favorite </span>artist.
-          </p>
-        </div>
-              </div>
-              <div className="flex justify-center py-2 dark:text-white
-              ">
-              <footer>
-                Erick adikah © {new Date().getFullYear()}
-              </footer>
-              </div>
-              </section>
-              </main>
+            <div
+              className="w-full x1:w-1/2 fle flex-col gap-6 
+          items-end text-right x1:-m-1-16 z-10"
+            >
+              <p className="font-titleFont text-textGreen text-center text-sm tracking-wide dark:text-white">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold text-center dark:text-white">
+                Art Galary
+              </h3>
+              <p className="bg-[#A09CA4] text-sm md:text-base p-2 md:p-6 rounded-md  dark:text-white">
+                Art Galary is a website where you can find all the art work of
+                your favorite artist. and you can also buy the art work from the
+                website. you can also find the art work of your{" "}
+                <span className="text-teal-500">favorite </span>artist.
+              </p>
             </div>
+            <a href="https://digital-credentials-wallet.vercel.app/">
+              <div className="basis-1/2 flex-2">
+                <Image
+                  className="w-full h-full object-contain justify-between"
+                  width={"80%"}
+                  height={"80%"}
+                  layout="responsive"
+                  src={web8}
+                  alt="project7"
+                />
+              </div>
+            </a>
+            <div
+              className="w-full x1:w-1/2 fle flex-col gap-6 
+              items-end text-right x1:-m-1-16 z-10"
+            >
+              <p className="font-titleFont text-textGreen text-center text-sm tracking-wide dark:text-white">
+                Featured Project
+              </p>
+              <h3 className="text-2xl font-bold text-center dark:text-white">
+                Digital Wallet
+              </h3>
+              <p className="bg-[#A09CA4] text-sm md:text-base p-2 md:p-6 rounded-md  dark:text-white">
+                the effortless way to manage your job application documents. Store your CV, certificates, images,
+                and cover letters in one secure place. Retrieve them with ease when applying for job vacancies.
+                 Simplify your application process and impress employers. 
+                Try Digital Wallet today and experience a streamlined approach to job hunting.
+                <span className="text-teal-500">
+                  and you can also share your data with other
+                </span>{" "}
+                people.
+                <p>
+                  {" "}
+                  Stack used: <span className="text-teal-500">MERN Stack</span>
+                </p>
+              </p>
+            </div>
+            <div
+              className="w-full x1:w-1/2 fle flex-col gap-6 
+              items-end text-right x1:-m-1-16 z-10"
+            >
+              <p className="font-titleFont text-textGreen text-center text-sm tracking-wide dark:text-white">
+                Feature Projects
+              </p>
+              <h3 className="text-2xl font-bold text-center dark:text-white">
+                Traffi Watch
+              </h3>
+              <p className="bg-[#A09CA4] text-sm md:text-base p-2 md:p-6 rounded-md  dark:text-white">
+                 your go-to traffic monitoring and navigation website.
+                <span className="text-teal-500"> informed about city traffic and effortlessly navigate to your destination.</span>S
+                Get real-time updates from fellow drivers, ensuring a smooth journey. 
+                 Try Traffi Watch today and make your commute a breeze.
+              </p>
+            </div>
+          </div>
+          <div
+            className="flex justify-center py-2 dark:text-white
+              "
+          >
+            <footer>Erick adikah © {new Date().getFullYear()}</footer>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
